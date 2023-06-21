ab6a03752278e514a01974727292adb59e563658
Damiem Mclaurin <damiemmclaurin@Damiems-MacBook-Pro.local>
2023-06-20 23:04:37 -0400

    working on models.

---
+++ fantastic-umbrella-main/Develop/models/ProductTag.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+Product.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+  
+    product_id: {
+      type: DataTypes.INTERGER,
+      refrences: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+
+    tag_id: {
+      type:DataTypes.INTERGER,
+      refrences: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = Product;
